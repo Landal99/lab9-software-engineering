3bd43c9b7df556f6e5d2db89dcdcd281351ec242
Landal99 <l.daley@ufl.edu>
2024-07-24 19:26:19 -0500

    initial encoding and main complete

--- main.py
+++ main.py
@@ -1,16 +1,45 @@
-# This is a sample Python script.
+"""
+COP3502C Lab 9 Software Engineering
+Lance Daley
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+Understand purpose and usefulness of version control.
+"""
+
+def encode(password):
+    new_password = ""
+    for char in password:
+        new_password += str((int(char) + 3) % 10)
+    return new_password
+
+def decode(password):
+    # coding for Jason
+    pass
+
+def display_menu():
+    print("\nMenu")
+    print("-------------")
+    print("1. Encode")
+    print("2. Decode")
+    print("3. Quit")
+    print()
+
+def main():
+    while True:
+        display_menu()
+        selection = int(input("Please enter an option: "))
+        if selection == 1:
+            password = input("Please enter the password to encode: ")
+            new_password = encode(password)
+            print("Your password has been encoded and stored!")
+        elif selection == 2:
+            print("placeholder for Jason")
+            # coding for Jason
+        elif selection == 3:
+            break
+        else:
+            print("Please enter a valid option")
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    main()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
